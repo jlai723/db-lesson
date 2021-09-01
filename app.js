7b16a0946595e1df02ff46bcf7fbdcb08f792fa5
Jackie Lai <jlai@bbbsci.org>
2021-08-31 20:00:48 -0400

    Went through different types of db associations

--- app.js
+++ app.js
@@ -18,17 +18,64 @@ const User = sequelize.define("User", {
     }
 });
 /*
-* One to One Relationships
+* One to One Relations
 */
+const Profile = sequelize.define("Profile", {
+    birthday: {
+        type: DataTypes.DATE,
+    }
+});
+
+User.hasOne(Profile, {
+    onDelete: "CASCADE",
+});
+Profile.belongsTo(User);
 
 /*
-* One to Many Relationships
+* One to Many Relations
 */ 
+const Order = sequelize.define("Order", {
+    shipDate: {
+        type: DataTypes.DATE,
+    }
+});
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 /*
-* Many to Many Relationships
+* Many to Many Relations
 */ 
+const Class = sequelize.define("Class", {
+    className: {
+        type: DataTypes.STRING,
+    },
+    startDate: {
+        type: DataTypes.DATE,
+    }
+});
+
+User.belongsToMany(Class, { through: 'Users_Classes' });
+Class.belongsToMany(User, { through: 'Users_Classes' });
 
 ((async() => {
-    await sequelize.sync();
+    await sequelize.sync({force: true});
+
+    let myUser = await User.create({
+        username: "Jackie123"
+    });
+    let myProfile = await Profile.create({
+        birthday: new Date() 
+    });
+    console.log(await myUser.getProfile());
+    await myUser.setProfile(myProfile);
+    console.log(await myUser.getProfile());
+
+    let resultUser = await User.findOne({
+        where: {
+            id: 1
+        }
+    });
+    console.log(await resultUser.getProfile());
+
 })());
